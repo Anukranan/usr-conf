158e70bc83019d9ef342b32b3df7c1d3ef6fa32c
Anukranan <anukranan@protonmail.com>
2024-01-08 13:06:28 -0500

    A few improvements and increased consistency across configuration files.

--- .config/sway/scripts/colorgen.sh
+++ .config/sway/scripts/colorgen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# This small script runs wal on the given input
-# and then generates a GTK theme using wpg,
-# finally ensuring that the proper theme and
-# font is applied as well.
+# This small script runs wal on the given input and then generates a GTK
+# theme using wpg, finally ensuring that the proper theme and font is applied as well.
+
+# If you run it in exec_always, it completely lags the system out.
 
 wal -i $1 && wpg -a $1 && wpg -ns $1 && \
 gsettings set org.gnome.desktop.interface gtk-theme FlatColor && \
