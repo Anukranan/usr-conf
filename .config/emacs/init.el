035009c80ad1a24383b3fc28e399f3260920cb71
Anukranan <anukranan@protonmail.com>
2024-01-16 23:51:44 -0500

    More large changes to Emacs and some minor edits to the shell rc and waybar styling.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -3,15 +3,64 @@
 ;; init.el    |
 ;;============/
 
+;; * TODO:
+;;   + Fix the set-reasonable-style-i function.
+;;   + Look over all of the hooks to ensure efficiency and cleanliness.
+;;   + Consider using eglot instead of lsp-mode.
+;;   + Consider using corfu instead of company.
+;;   + Consider using project.el instead of projectile.
+;;   + Consider using ido instead of ivy.
+;;   + Consider using gud instead of dap-mode.
+;;   + Integrate desktop environment.
+;;     - File manager (dired + dired extensions).
+;;     - Use eshell most of the time?
+;;     - Control package manager.
+;;     - Method to play and view media, including querying youtube videos.
+;;     - Method to play spotify?
+;;     - EWW web browser.
+;;     - Org mode for... organization and notes.
+;; I would still like to reduce the amount of packages I use and their
+;; complexity, in order to maintain a fast emacs. However, I would also like
+;; to have a comfortable, complete system.
+
+;;============================================
+;; Initialization.
+;;============================================
+
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "* Booted in %s seconds with %d garbage collections."
+                     (emacs-init-time "%.2f") gcs-done)))
+
+(setq gc-cons-threshold 100000000
+      read-process-output-max (* 1024 1024))
+
 (setq user-init-file "~/.config/emacs/init.el"
-      user-emacs-directory "~/.config/emacs")
+      user-emacs-directory "~/.config/emacs"
+      inhibit-startup-message t
+      initial-scratch-message nil
+      initial-major-mode 'emacs-lisp-mode
+      visible-bell t)
+
+(scroll-bar-mode -1)
+(tool-bar-mode   -1)
+(tooltip-mode    -1)
+(menu-bar-mode   -1)
+(set-fringe-mode  5)
+
+(prefer-coding-system 'utf-8-unix)
+(set-language-environment "UTF-8")
+
 
 ;;============================================
 ;; Functions.
 ;;============================================
 
+;; Internally defined functions are post-pended with '-i'. Functions sourced
+;; externally without change are not.
+
 ;;-------------
-;; Mode-specific.
+;; Settings.
 ;;-------------
 
 (defun c-lineup-arglist-tabs-only (ignored)
@@ -23,37 +72,22 @@
     (* (max steps 1)
        c-basic-offset)))
 
-;;-------------
-;; Settings.
-;;-------------
-
-(defun set-tabs-i (width)
-  "Set tab width."
+(defun linux-tab-style-i (width)
+  "Indent with tabs and set linux style for cc-mode."
   (interactive "nTab width: ")
-  (setq-default tab-width width
-                tab-stop-width width
-                tab-stop-list (number-sequence width 120 width)
-                c-basic-indent width
-                c-basic-offset width))
-
-;; TODO: Make this work, and possibly make it apply automatically when
-;; it detects a large amount of tabs in a file.
-(defun set-reasonable-style-i ()
-  "Set buffer to sane values (use 8 width tabs, line arguments up by tabs)."
-  (interactive)
-  (lambda ()
-    (progn
-      (setq indent-tabs-mode t
-            c-default-style "linux")
-      (set-tabs-i 8)
-      (c-add-style
-       "linux-tabs-only"
-       '("linux" (c-offsets-alist
-                  (arglist-cont-nonempty
-                   c-lineup-gcc-asm-reg
-                   c-lineup-arglist-tabs-only))))
-      (c-set-style "linux-tabs-only"))))
-
+  (progn
+    (setq indent-tabs-mode t
+          tab-width width
+          c-basic-indent width
+          c-basic-offset width
+          c-default-style "linux")
+    (c-add-style
+     "linux-tabs-only"
+     '("linux" (c-offsets-alist
+                (arglist-cont-nonempty
+                 c-lineup-gcc-asm-reg
+                 c-lineup-arglist-tabs-only))))
+    (c-set-style "linux-tabs-only")))
 
 ;;-------------
 ;; Utiltity.
@@ -70,12 +104,9 @@
 ;; Package management.
 ;;============================================
 
-;;-------------
-;; Archives.
-;;-------------
-
 (require 'package)
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil
+      package-native-compile t)
 (add-to-list 'package-archives '("gnu"    . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
 (add-to-list 'package-archives '("melpa"  . "https://melpa.org/packages/"))
@@ -84,6 +115,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (eval-and-compile
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
@@ -91,42 +123,30 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;;-------------
-;; Package list.
-;;-------------
-
-;; Development environment.
-(use-package lsp-mode)
-(use-package lsp-ui)
-(use-package lsp-ivy)
-(use-package company)
-(use-package company-box)
-(use-package projectile)
-(use-package counsel-projectile)
-(use-package magit)
-
-(use-package lsp-java)
-(use-package lsp-haskell)
-(use-package lsp-latex)
-(use-package auctex)
-(use-package slime)
-(use-package ess)
-
-(use-package ewal)
-(use-package ewal-spacemacs-themes)
-
 
 ;;============================================
+;; Configuration.
+;;============================================
+
+;;-------------
+;; General settings.
+;;-------------
+
+(setq-default tab-width 2
+              indent-tabs-mode nil
+              backward-delete-char-untabify-method 'hungry
+              display-fill-column-indicator-column 80
+              column-number-mode t)
+
+(add-hook-i '(prog-mode-hook) '(lambda () (setq show-trailing-whitespace t)))
+(add-hook-i '(prog-mode-hook text-mode-hook org-mode-hook)
+            'display-line-numbers-mode)
+(add-hook-i '(prog-mode-hook text-mode-hook org-mode-hook)
+            'display-fill-column-indicator-mode)
+
+;;-------------
 ;; Theming.
-;;============================================
-
-(setq inhibit-startup-message t
-      visible-bell t)
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
-(menu-bar-mode   -1)
-(set-fringe-mode 5)
+;;-------------
 
 (set-frame-font "Termsyn 11" nil t)
 
@@ -142,40 +162,15 @@
             (load-theme 'ewal-spacemacs-classic t)
             (enable-theme 'ewal-spacemacs-classic)))
 
-
-;;============================================
-;; Basic.
-;;============================================
-
 ;;-------------
-;; General.
+;; Desktop environment.
 ;;-------------
 
-(setq-default indent-tabs-mode nil
-              backward-delete-char-untabify-method 'hungry
-              column-number-mode t
-              display-fill-column-indicator-column 80
-              show-trailing-whitespace nil)
 
-(add-hook-i '(prog-mode-hook) '(lambda () (setq show-trailing-whitespace t)))
-(add-hook-i '(prog-mode-hook text-mode-hook org-mode-hook)
-            'display-line-numbers-mode)
-(add-hook-i '(prog-mode-hook text-mode-hook org-mode-hook)
-            'display-fill-column-indicator-mode)
-
-(set-tabs-i 2)
-(prefer-coding-system 'utf-8-unix)
-(set-language-environment "UTF-8")
 
 ;;-------------
-;; IDE configuration.
+;; Development environment.
 ;;-------------
-
-;; Performance suggestions from the lsp-mode website.
-(setq gc-cons-threshold 100000000
-      read-process-output-max (* 1024 1024)
-      lsp-idle-delay 0.500
-      lsp-log-io nil)
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
@@ -210,17 +205,6 @@
   (company-idle-delay 0.0))
 (use-package company-box
   :hook (company-mode . company-box-mode))
-
-(use-package projectile
-  :diminish projectile-mode
-  :config (projectile-mode)
-  :custom ((projectile-completion-system 'ivy))
-  :bind-keymap ("C-c p" . projectile-command-map)
-  :init (setq projectile-switch-project-action #'projectile-dired)
-  (when (file-directory-p "~/var/doc/src")
-    (setq projectile-project-search-path '("~/var/doc/src"))))
-(use-package counsel-projectile
-  :config (counsel-projectile-mode))
 
 
 ;;============================================
@@ -272,7 +256,7 @@
  '(custom-safe-themes
    '("e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "e4a702e262c3e3501dfe25091621fe12cd63c7845221687e36a79e17cf3a67e0" "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "e28d05d3fdc7839815df9f4e6cebceb4a0ca4ed2371bee6d4b513beabee3feb7" "edf5e3ea8b3bbb4602feef2dfac8a6d5dae316fb78e84f360d55dfda0d37fa09" default))
  '(package-selected-packages
-   '(auctex slime counsel-projectile projectile company-box ewal ess lsp-latex lsp-java lsp-haskell magit company lsp-ivy lsp-ui lsp-mode ewal-spacemacs-themes))
+   '(sway auctex slime company-box ewal ess lsp-latex lsp-java lsp-haskell magit company lsp-ivy lsp-ui lsp-mode ewal-spacemacs-themes))
  '(warning-suppress-log-types '((comp)))
  '(warning-suppress-types '((comp))))
 (custom-set-faces
