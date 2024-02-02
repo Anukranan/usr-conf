00ea0a97391fa97158e27ec3d938bc2c054f0f72
Anukranan <153703132+Anukranan@users.noreply.github.com>
2024-02-02 13:33:57 -0500

    Modify a bunch of files and add lf.

--- .local/bin/theme-gen.sh
+++ .local/bin/theme-gen.sh
@@ -3,9 +3,9 @@
 # Script to unify applying wal and GTK themes (wpg), light or dark, and with
 # internal or generated wal color schemes.
 
-#--------------------
+#---------------
 # Variables.
-#--------------------
+#---------------
 
 ERROR_MSG=$'Error: invalid input\n'
 HELP_MSG=$'Usage: theme-gen.sh [options] [wallpaper/theme]'
@@ -13,54 +13,55 @@ HELP_MSG=$'Usage: theme-gen.sh [options] [wallpaper/theme]'
 THEME_COLOR=$1
 THEME_FILE=$2
 
-#--------------------
+
+#---------------
 # Functions.
-#--------------------
+#---------------
 
 # Use an internal pywal theme.
 theme_init() {
-  wal $1 --theme $3
-  wpg-install.sh -g -i
-  wpg $2 --theme $3
+	wal $1 --theme $3
+	wpg-install.sh -g -i
+	wpg $2 --theme $3
 }
 
 # Use a generated wallpaper theme.
 gen_init() {
-  wal $1 -i $3
-  wpg-install.sh -g -i
-  wpg $2 -a $3
-  wpg $2 -ns $3
+	wal $1 -i $3
+	wpg-install.sh -g -i
+	wpg $2 -a $3
+	wpg $2 -ns $3
 }
 
 die() {
-  echo $1
-  echo $2
-  exit 1
+	echo $1
+	echo $2
+	exit 1
 }
 
 
-#--------------------
+#---------------
 # Main.
-#--------------------
+#---------------
 
 if [ ! $THEME_COLOR ] || [ ! $THEME_FILE ]; then
-  die "$ERROR_MSG" "$HELP_MSG"
+	die "$ERROR_MSG $HELP_MSG"
 fi
 
 if [ $THEME_COLOR = "dark" ]; then
-  ARGS_WAL=""
-  ARGS_WPG=""
-  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+	ARGS_WAL=""
+	ARGS_WPG=""
+	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 elif [ $THEME_COLOR = "light" ]; then
-  ARGS_WAL="-l"
-  ARGS_WPG="-L"
-  gsettings set org.gnome.desktop.interface color-scheme prefer-light
+	ARGS_WAL="-l"
+	ARGS_WPG="-L"
+	gsettings set org.gnome.desktop.interface color-scheme prefer-light
 else
-  die "$ERROR_MSG" "$HELP_MSG"
+	die "$ERROR_MSG" "$HELP_MSG"
 fi
 
 if [ -r $THEME_FILE ]; then
-  gen_init "$ARGS_WAL" "$ARGS_WPG" "$THEME_FILE"
+	gen_init "$ARGS_WAL" "$ARGS_WPG" "$THEME_FILE"
 else
-  theme_init "$ARGS_WAL" "$ARGS_WPG" "$THEME_FILE"
+	theme_init "$ARGS_WAL" "$ARGS_WPG" "$THEME_FILE"
 fi
